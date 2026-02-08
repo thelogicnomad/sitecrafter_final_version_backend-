b51052974ac942b9e87ef907fa526f0e21a41eae
Chirag s <chirag748317@gmail.com>
2026-02-08 19:06:00 +0530

    added backend url

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const google_api = "https://monticulate-colleen-strainless.ngrok-free.dev/"
+export const google_api = "https://monticulate-colleen-strainless.ngrok-free.dev"
 // Local dev URL
 //export const google_api = "http://localhost:3000"
 export const frontend_config = "https://sitecrafter.vercel.app"
