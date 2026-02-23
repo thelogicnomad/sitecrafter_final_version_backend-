8a41f65b49bf7a757b008ae9a1bd8d55efe1df06
Chirag s <chirag748317@gmail.com>
2026-02-23 19:54:23 +0530

    Backend url modified with new one with SSL certificate

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const google_api = "https://sitecrafter-final-version-backend.vercel.app"
+export const google_api = "https://sitecrafter-api.duckdns.org"
 export const frontend_config = "https://sitecrafter.vercel.app"
