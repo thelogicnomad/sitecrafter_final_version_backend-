94d1b69dfe77ee8d019079a7c023608a21699cdb
Chirag s <chirag748317@gmail.com>
2026-02-23 17:14:13 +0530

    changed google redirection url

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,2 @@
-export const google_api = "https://monticulate-colleen-strainless.ngrok-free.dev"
-// Local dev URL
-//export const google_api = "http://localhost:3000"
+export const google_api = "https://sitecrafter-final-version-backend.vercel.app"
 export const frontend_config = "https://sitecrafter.vercel.app"
-//export const frontend_config="http://localhost:5173/"
